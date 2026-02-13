1d05827c5887f9dc4160d1af5113e2f0f6fac50a
Vikas Manjunath <vikasgm1@outlook.com>
2026-02-13 17:47:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,12 +9,16 @@ const nextConfig = {
       },
     ],
   },
-  // This forces the build to pass even if there are small errors
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
   },
 };
 
